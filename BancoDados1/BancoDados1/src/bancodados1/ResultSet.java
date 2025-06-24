f8014a5ab1e03e960211147478dc038d8646645e
Dev_Layanne <layannesousalay@gmail.com>
2025-06-24 11:53:20 -0300

    Add files via upload

---
+++ BancoDados1/BancoDados1/src/bancodados1/ResultSet.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bancodados1;
+
+/**
+ *
+ * @author Layanne55896236
+ */
+class ResultSet {
+
+    boolean next() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    Object getString(String id_login) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void close() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ BancoDados1/BancoDados1/src/bancodados1/TelaResetarSenha.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bancodados1;
+
+/**
+ *
+ * @author Layanne55896236
+ */
+class TelaResetarSenha {
+
+    class codigo {
+
+        static void setText(String codigoUsuario) {
+            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        }
+
+        public codigo() {
+        }
+    }
+
+    class usuario {
+
+        static void setText(String text) {
+            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        }
+
+        public usuario() {
+        }
+    }
+    
+}
